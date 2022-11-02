d9c39131d26d4ebcf1d9adf85634c734ca706c57
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2022-11-02 17:13:06 +0100

    Fixed weekday formatting errors caused by me ;)

--- frontend/src/components/DailyWeather/DailyWeather.jsx
+++ frontend/src/components/DailyWeather/DailyWeather.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
-function DailyWeather({ dateNumInitial, dayIcon, tempHigh, tempLow }) {
-  let dateNum = new Date(dateNumInitial * 1000);
-  dateNum.getDay();
+function DailyWeather({ dateNum, dayIcon, tempHigh, tempLow }) {
+  let dateNumFormatted = new Date(dateNum * 1000);
+  dateNumFormatted.getDay();
   const options = { weekday: "short" };
-  dateNum = Intl.DateTimeFormat("en-US", options).format(dateNum);
+  dateNumFormatted = Intl.DateTimeFormat("en-US", options).format(
+    dateNumFormatted
+  );
 
   return (
     <div>
       <img src={dayIcon} alt="icon" />
-      <h2>{dateNum}</h2>
+      <h2>{dateNumFormatted}</h2>
       <h2>{tempHigh}°C</h2>
       <h2>{tempLow}°C</h2>
     </div>
