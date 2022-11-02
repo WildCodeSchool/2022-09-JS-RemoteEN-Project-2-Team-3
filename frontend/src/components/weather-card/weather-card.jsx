00f4114508205f56554e1f7e57a2d9d950e32025
Bukabuka321 <20uva02@gmail.com>
2022-11-02 16:40:14 +0100

    created a weather-card with main info

--- frontend/src/components/weather-card/weather-card.jsx
+++ frontend/src/components/weather-card/weather-card.jsx
@@ -1,15 +1,18 @@
 import "./weather-card.css";
 import axios from "axios";
 import React from "react";
+import humidity from "./Icons/humidity.png";
+import atmospheric from "./Icons/atmospheric.png";
+import wind from "./Icons/wind.png";
 
 function WeatherCard() {
   const [weatherData, setWeatherData] = React.useState();
   // const [air, setAir] = React.useState();
   const [location, setLocation] = React.useState("London");
 
-  const API_KEY = import.meta.env.VITE_WEATHERSTACK_API_KEY;
+  const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
 
-  const url = `http://api.weatherstack.com/current?access_key=${API_KEY}&query=${location}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`;
 
   const searchLocation = () => {
     axios
@@ -20,21 +23,26 @@ function WeatherCard() {
       });
   };
 
-  // const urlTwo = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${weatherData.location.lat}&lon=${weatherData.location.lon}&appid={API key}`;
-  // axios
-  //   .get(urlTwo)
-  //   .then((response) => response.data)
-  //   .then((data) => {
-  //     setAir(data);
-  //   });
+  // const searchAir = () => {
+  //   axios
+  //     .get(newUrl)
+  //     .then((response) => response.data)
+  //     .then((data) => {
+  //       setAir(data);
+  //     });
+  // };
 
   React.useEffect(searchLocation, []);
 
   const keyDownHandler = (event) => {
     if (event.key === "Enter") {
       searchLocation();
+      // searchAir();
     }
   };
+
+  const date = new Date();
+  const setDate = date.toDateString();
 
   return (
     <div className="weather-card">
@@ -43,31 +51,36 @@ function WeatherCard() {
           value={location}
           onChange={(event) => setLocation(event.target.value)}
           onKeyDown={keyDownHandler}
-          placeholder="search"
+          placeholder="Search city"
           type="text"
         />
       </div>
       {weatherData && (
         <>
           <div className="city-name">
-            <p>{weatherData.location.name}</p>
+            <p>{weatherData.name}</p>
           </div>
-          <div className="today-date">{weatherData.location.localtime}</div>
+          <div className="today-date">{setDate}</div>
           <div className="current-temperature">
-            <p>{Math.round(weatherData.current.temperature)}°C</p>
-            <p>Feels like: {Math.round(weatherData.current.feelslike)}°C</p>
-          </div>
-          <div className="wind-speed">
-            Wind: {Math.round(weatherData.current.wind_speed)}km/h
+            <p>{Math.round(weatherData.main.temp)}°C</p>
+            <p>Feels like: {Math.round(weatherData.main.feels_like)}°C</p>
           </div>
           <div className="extra-info">
             <ul>
-              <li>Humidity: {weatherData.current.humidity}%</li>
-              <li>UV index: {weatherData.current.uv_index}</li>
-              {/* <li>Air quality: {air.data.main.aqi}</li> */}
-              <li>Pressure: {weatherData.current.pressure} hPa</li>
+              <li>
+                Humidity: {weatherData.main.humidity}%
+                <img src={humidity} alt="humidity" />
+              </li>
+              <li>
+                Wind: {Math.round(weatherData.wind.speed)} km/h
+                <img src={wind} alt="wind" />
+              </li>
+              {/* <li>Air quality: {air.list.main.aqi}</li> */}
+              <li>
+                Pressure: {weatherData.main.pressure} hPa
+                <img src={atmospheric} alt="atmospheric" />
+              </li>
             </ul>
-            {/* <img alt="icon" src="./Icons/humidity 1.png" /> */}
           </div>
         </>
       )}
