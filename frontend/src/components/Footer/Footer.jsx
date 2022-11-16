3efc167ec1079fad2cceb5553dafdba541fd19c6
Valeriia.Kurylko <reviliya@gmail.com>
2022-11-16 13:28:11 +0100

    basic structure for footer added

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer-body">
+      <p className="provided-by"> Data privided by OpenWeather</p>
+      <p className="made-with-love"> Made with love </p>
+    </div>
+  );
+}
+
+export default Footer;
