04245dc9ff639cdfb03c6fd39df2d3a2e477fe89
Valeriia.Kurylko <reviliya@gmail.com>
2022-11-16 17:46:45 +0100

    popup cs fixed

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,10 +1,28 @@
 import "./Footer.css";
+import React from "react";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 function Footer() {
   return (
     <div className="footer-body">
       <p className="provided-by"> Data provided by OpenWeather</p>
-      <p className="made-with-love"> Made with 💙 </p>
+
+      <Popup
+        className="pop-up"
+        position="top center"
+        trigger={<p className="made-with-love"> Made with 💙 </p>}
+      >
+        <div className="my-popup-content">
+          <p>Development Team 👨‍💻</p>
+          <ul>
+            <li>Olga Dobrovolska</li>
+            <li>Vitalii Yurchenko</li>
+            <li>Olena Shuliakovska</li>
+            <li>Valeria Kurylko</li>
+          </ul>
+        </div>
+      </Popup>
     </div>
   );
 }
