284f0977621019be188223ab86c0d5e94762b98a
OLGA DOBROVOLSKA <3250186@gmail.com>
2022-11-23 14:10:37 +0200

    added the button in Weekly Weather section only for mobile version

--- frontend/src/components/DailyWeather/Weather.jsx
+++ frontend/src/components/DailyWeather/Weather.jsx
@@ -1,24 +1,40 @@
+import React from "react";
 import "./Weather.css";
 import DailyWeather from "./DailyWeather";
 
 function Weather({ dailyWeather }) {
+  const [isShown, setIsShown] = React.useState(true);
   return (
-    <div className="weekly_forecast">
-      {dailyWeather != null ? (
-        dailyWeather.daily.slice(0, 7).map((day) => {
-          return (
-            <DailyWeather
-              key={day.dt}
-              dateNum={day.dt}
-              dayIcon={`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
-              tempHigh={Math.round(day.temp.max)}
-              tempLow={Math.round(day.temp.min)}
-              pop={Math.round(day.pop)}
-            />
-          );
-        })
-      ) : (
-        <h2>Loading...</h2>
+    <div>
+      <div className="optional_detail">
+        <h2>Weekly forecast</h2>
+        <button
+          type="button"
+          onClick={() => setIsShown(!isShown)}
+          id="mobile_title"
+        >
+          {isShown ? "    ❮ " : "... ❯ "}
+        </button>
+      </div>
+      {isShown && (
+        <div className="weekly_forecast">
+          {dailyWeather != null ? (
+            dailyWeather.daily.slice(0, 7).map((day) => {
+              return (
+                <DailyWeather
+                  key={day.dt}
+                  dateNum={day.dt}
+                  dayIcon={`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
+                  tempHigh={Math.round(day.temp.max)}
+                  tempLow={Math.round(day.temp.min)}
+                  pop={Math.round(day.pop)}
+                />
+              );
+            })
+          ) : (
+            <h2>Loading...</h2>
+          )}
+        </div>
       )}
     </div>
   );
